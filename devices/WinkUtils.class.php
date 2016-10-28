e7f5966de323d3d0f06460858a19912d2bfd34de
Daniel Díaz <yosoy@danieldiaz.org>
2016-10-28 12:58:44 -0500

    wink: Add GET method for the API.
    Signed-off-by: Daniel Díaz <yosoy@danieldiaz.org>

--- devices/WinkUtils.class.php
+++ devices/WinkUtils.class.php
@@ -104,6 +104,21 @@ class WinkUtils extends device
 		return $info['idfield'];
 	}
 
+	static function api_get($endpoint)
+	{
+		global $config;
+		$token = $config['wink']['token_access'];
+
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, WinkUtils::$URL . $endpoint);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+		curl_setopt($ch, CURLOPT_HEADER, FALSE);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json", "Authorization: Bearer " . $token));
+		$response = curl_exec($ch);
+		curl_close($ch);
+		return $response;
+	}
+
 	static function api_put($endpoint, $data)
 	{
 		global $config;
